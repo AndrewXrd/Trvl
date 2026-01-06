fd6b2deba8cbb68767028b5fd8421312e4076cf6
Anton Ignacious Andrew <andrewkavin30@gmail.com>
2026-01-06 11:49:20 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
-import './css/App.css'
 
 function App() {
 
   return (
     <div>
-    <NavBar />
-    <main className='main'>
-    <Routes>
-        <Route path='/' exact Component={Home} />
-    </Routes>
-    </main>
+      <NavBar />
+      <main className='main'>
+        <Routes>
+          <Route path='/' exact Component={Home} />
+        </Routes>
+      </main>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,49 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../css/NavBar.css'
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
 
-function NavBar(){
+function NavBar() {
 
-  return (
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
-    <nav className='navBar'>
-        <div className='navBar-container'>
-            <Link to='/' className='navBar-logo-text'>
-                <img src={logo} className='logo' />   Trvl
-            </Link>
-            <div className='navBar-items-list' >
-                <ul>
-                    <li className='navBar-item'>
-                        <Link to='/' className='navBar-links'>Home</Link>
-                    </li>
-                    <li className='navBar-item'>
-                        <Link to='/' className='navBar-links'>Services</Link>
-                    </li>
-                    <li className='navBar-item'>
-                        <Link to='/' className='navBar-links'>About</Link>
-                    </li>
-                </ul>
+    return (
+        <nav className='navBar'>
+            <div className='navBar-container'>
+                <Link to='/' className='navBar-logo-text' onClick={closeMobileMenu}>
+                    <img src={logo} className='logo' alt="Trvl Logo" />   Trvl
+                </Link>
 
-                <div className='navBar-signup-btn'>Sign Up</div>
+                <div className='menu-icon' onClick={handleClick}>
+                    {click ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                    ) : (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+                    )}
+                </div>
+
+                <div className={click ? 'navBar-items-list active' : 'navBar-items-list'}>
+                    <ul>
+                        <li className='navBar-item'>
+                            <Link to='/' className='navBar-links' onClick={closeMobileMenu}>Home</Link>
+                        </li>
+                        <li className='navBar-item'>
+                            <Link to='/' className='navBar-links' onClick={closeMobileMenu}>Services</Link>
+                        </li>
+                        <li className='navBar-item'>
+                            <Link to='/' className='navBar-links' onClick={closeMobileMenu}>About</Link>
+                        </li>
+                    </ul>
+
+                    <div className='navBar-signup-btn'>Sign Up</div>
+                </div>
+
             </div>
 
-        </div>
-
-    </nav>
-
-  )
+        </nav>
+    )
 }
 
 export default NavBar
